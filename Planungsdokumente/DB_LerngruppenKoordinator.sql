383f8f4d0065661d36db88a272a390ba8c1cf32e
Rollo142 <jonasbecker123@icloud.com>
2025-11-11 13:22:55 +0100

    SQL Code angepasst

--- Planungsdokumente/DB_LerngruppenKoordinator.sql
+++ Planungsdokumente/DB_LerngruppenKoordinator.sql
@@ -1,8 +1,15 @@
 use master;
-drop DATABASE LerngruppenKoordinatorDB;
+IF exists (select * from sys.databases where name = 'LerngruppenKoordinatorDB')
+BEGIN 
+	DROP DATABASE LerngruppenKoordinatorDB;
+END
 
 CREATE DATABASE LerngruppenKoordinatorDB;
 USE LerngruppenKoordinatorDB;
+
+IF OBJECT_ID('Terminkalender', 'U') IS NOT NULL DROP TABLE Terminkalender;
+IF OBJECT_ID('LERNGRUPPE', 'U') IS NOT NULL DROP TABLE LERNGRUPPE;
+IF OBJECT_ID('BENUTZER', 'U') IS NOT NULL DROP TABLE BENUTZER;
 
 CREATE TABLE BENUTZER (
 	ID INT PRIMARY KEY IDENTITY(1,1),
@@ -28,23 +35,44 @@ CREATE TABLE Terminkalender (
 	TERMIN DATETIME DEFAULT GETDATE(),
 	PRIMARY KEY (BENUTZERID, LERNGRUPPENID),
 	FOREIGN KEY (BENUTZERID) REFERENCES BENUTZER(ID),
-	FOREIGN KEY (LERNGRUPPENID) REFERENCES LERNGRUPPE(ID),
+	FOREIGN KEY (LERNGRUPPENID) REFERENCES LERNGRUPPE(ID)
 	);
 
 INSERT INTO BENUTZER (NAME, ADRESSE, PLZ, STUDIENGANG, FACHSEMESTER)
 VALUES 
 ('Anna Schmidt', 'Hauptstraﬂe 12', '66111', 'Informatik', 3),
 ('Jonas Becker', 'Bergstraﬂe 5', '66115', 'Maschinenbau', 2),
-('Laura Klein', 'Marktplatz 8', '66119', 'Psychologie', 4);
+('Laura Klein', 'Marktplatz 8', '66119', 'Psychologie', 4),
+('Max M¸ller', 'Schulstraﬂe 7', '66113', 'BWL', 1),
+('Sophie Meier', 'Gartenweg 3', '66121', 'Medizin', 2);
 
 INSERT INTO LERNGRUPPE (NAME, FACH, ADRESSE, PLZ)
 VALUES 
 ('Algo-Team', 'Algorithmen und Datenstrukturen', 'Uni Campus Geb‰ude E1', '66123'),
 ('Maschbau-Gruppe', 'Technische Mechanik', 'HTW Saar Geb‰ude B', '66117'),
-('PsychoLab', 'Kognitive Psychologie', 'Uni Campus Geb‰ude C3', '66125');
+('PsychoLab', 'Kognitive Psychologie', 'Uni Campus Geb‰ude C3', '66125'),
+('BWL-Workshop', 'Betriebswirtschaftliche ‹bungen', 'Uni Campus Geb‰ude F2', '66122'),
+('Medizin-Gruppe', 'Anatomie und Physiologie', 'Uni Campus Geb‰ude G1', '66124');
 
 INSERT INTO Terminkalender (BenutzerID, LERNGRUPPENID, TERMIN)
 VALUES 
 (1, 1, '2025-11-02 10:00'),
 (2, 2, '2025-11-03 14:30'),
-(3, 3, '2025-11-04 09:00');
+(3, 3, '2025-11-04 09:00'),
+(4, 4, '2025-11-05 11:00'),
+(5, 5, '2025-11-06 13:30');
+
+
+
+SELECT 
+    b.ID AS BenutzerID,
+    b.NAME AS Benutzer,
+    b.STUDIENGANG,
+    l.ID AS LerngruppenID,
+    l.NAME AS Lerngruppe,
+    l.FACH,
+    t.TERMIN
+FROM Terminkalender t
+JOIN BENUTZER b ON t.BenutzerID = b.ID
+JOIN LERNGRUPPE l ON t.LERNGRUPPENID = l.ID
+ORDER BY t.TERMIN;
